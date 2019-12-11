b09ed200495c0fb920c483f0ad99937938f05b4b
DomenPigeon <domen.golob.px@gmail.com>
2019-12-11 16:19:23 +0100

    Gitignore restored

---
+++ 2019/Day02/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AdventLibrary;
+using Console = AdventLibrary.Console;
+
+namespace Day02 {
+    internal class Program {
+        public static void Main(string[] args) {
+            var input = ParseInput.ToArray("input");
+            var result = 0;
+            
+            Console.WriteLine(result);
+        }
+    }
+}
